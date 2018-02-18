28000d06d1d1f7110462ed4d578e7d012a2db273
Benjamin Dudok <BenjaminWD@gmail.com>
2018-02-18 15:25:49 +0100

    Moved function to get sprite data to app component because we only need to retrieve it once

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,9 +1,21 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import {} from 'vue-property-decorator';
+import { Watch } from 'vue-property-decorator';
+import * as types from '../store/types';
 
 @Component
 export default class App extends Vue
 {
     name: string = 'app';
+
+    mounted(): void
+    {
+        this.getSpriteData();
+    }
+
+    @Watch('route.params.categoryName')
+    getSpriteData(): void
+    {
+        this.$store.dispatch(types.GET_SPRITES);
+    }
 }
--- src/views/listing/listing.ts
+++ src/views/listing/listing.ts
@@ -1,22 +1,9 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { Watch } from 'vue-property-decorator';
-import * as types from 'src/store/types';
 
 @Component
 export default class Listing extends Vue
 {
-    mounted(): void
-    {
-        this.getSpriteData();
-    }
-
-    @Watch('route.params.categoryName')
-    getSpriteData(): void
-    {
-        this.$store.dispatch(types.GET_SPRITES);
-    }
-
     get sprites(): string
     {
         if (this.$route.params.categoryName)
